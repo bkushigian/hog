55089ae413bacdb7d964e33810dcfb8a77956079
bkushigian <bkushigian@gmail.com>
2017-11-03 20:29:24 +0000

    Enriched filesystem API
    Created calculate_absolute_path to recursively traverse parent directories until
    a root is found (in both File and Directory). Basic __eq__, __hash__, __repr__,
    __str__ methods for File and Directory, as well as some other methods

--- utils/filesystem.py
+++ utils/filesystem.py
@@ -2,16 +2,34 @@
 filesystem.py represents files and directories
 """
 
+import os
+
 class File:
     """ A file in the file system """
-    def __init__(self, name, contents, parent=None):
+    def __init__(self, name, contents='', parent=None):
         self.name = name
         self.contents = contents
         self.parent = parent
+        self.absolute_path = self.calculate_absolute_path()
+
+    def calculate_absolute_path(self):
+        return os.path.join(self.parent.absolute_path, self.name)
+
+    def set_parent(self, directory):
+        self.parent = directory
 
     def __eq__(self, other):
         """ This does not compare contents - only name and parent """
         return self.name == other.name and self.parent == other.parent
+
+    def __hash__(self):
+        return hash(hash(self.name, hash(self.parent)))
+
+    def __repr__(self):
+        return 'File(' + self.name + '/)'
+
+    def __str__(self):
+        return self.name
 
 
 class Directory:
@@ -22,6 +40,7 @@ class Directory:
         self.files = files
         self.directories = directories
         self.parent = parent
+        self.absolute_path = self.calculate_absolute_path()
 
         if files is None:
             self.files = []
@@ -32,12 +51,30 @@ class Directory:
         """ add a file to self """
         if file not in self.files:
             self.files.append(file)
+            file.set_parent(self)
 
     def add_directory(self, directory):
         """ add a directory to self """
         if directory not in self.directories:
             self.directories.append(directory)
 
+    def calculate_absolute_path(self):
+        if self.parent is None:
+            return os.path.join(self.name)
+        return os.path.join(self.parent.absolute_path, self.name)
+
+    def set_parent(self, directory):
+        self.parent = directory
+
     def __eq__(self, other):
         """ Compare directories """
         return self.name == other.name and self.parent == other.parent
+
+    def __hash__(self):
+        return hash(hash(self.name, hash(self.parent)))
+
+    def __repr__(self):
+        return 'Directory(' + self.name + '/)'
+
+    def __str__(self):
+        return self.name
