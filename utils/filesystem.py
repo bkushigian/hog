7b128fad80de43452b95f8d7bc3ee6ca58c783e4
bkushigian <bkushigian@gmail.com>
2017-11-03 20:12:04 +0000

    Basic API for filesystem

--- utils/filesystem.py
+++ utils/filesystem.py
@@ -9,6 +9,11 @@ class File:
         self.contents = contents
         self.parent = parent
 
+    def __eq__(self, other):
+        """ This does not compare contents - only name and parent """
+        return self.name == other.name and self.parent == other.parent
+
+
 class Directory:
     """ A directory in the file system """
 
@@ -17,3 +22,22 @@ class Directory:
         self.files = files
         self.directories = directories
         self.parent = parent
+
+        if files is None:
+            self.files = []
+        if directories is None:
+            self.directories = []
+
+    def add_file(self, file):
+        """ add a file to self """
+        if file not in self.files:
+            self.files.append(file)
+
+    def add_directory(self, directory):
+        """ add a directory to self """
+        if directory not in self.directories:
+            self.directories.append(directory)
+
+    def __eq__(self, other):
+        """ Compare directories """
+        return self.name == other.name and self.parent == other.parent
